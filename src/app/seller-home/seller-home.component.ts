7cc737a8b38c0026d425b39759c008a8e736ebe1
Zulkifal12 <mzulkifaluetp@gmail.com>
2022-10-30 10:56:09 -0700

    Upto FontAwosome

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,39 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { product } from '../data-type';
 import { ProductService } from '../services/product.service';
+import {faTrash} from "@fortawesome/free-solid-svg-icons"
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
-  styleUrls: ['./seller-home.component.css']
+  styleUrls: ['./seller-home.component.css'],
 })
 export class SellerHomeComponent implements OnInit {
-  productlist:undefined | product[]
-  deleteMessage:undefined |string;
-  constructor(private product:ProductService) { }
-  
+  productlist: undefined | product[];
+  deleteMessage: undefined | string;
+  Trashicon = faTrash;
+  constructor(private product: ProductService) {}
+
   ngOnInit(): void {
     this.list();
-    
   }
 
-  deleteProduct(id:number){
-    this.product.deleteProduct(id).subscribe((result)=>{
-      if(result){
-         this.deleteMessage = "Item Deleted Successfully"
-         this.list();
+  deleteProduct(id: number) {
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.deleteMessage ='Item Deleted Successfully';
+        this.list();
       }
-      setTimeout(()=>{
-        this.deleteMessage = undefined
-      },3000);
-    })
+      setTimeout(() => {
+        this.deleteMessage = undefined;
+      }, 3000);
+    });
   }
 
-  list(){
-    this.product.productlist().subscribe((result)=>{
+  list() {
+    this.product.productlist().subscribe((result) => {
       console.warn(result);
       this.productlist = result;
-     })
+    });
   }
-
 }
