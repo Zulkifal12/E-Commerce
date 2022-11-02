d5d283dd3b874fded186816451f0fd979bfc95dc
Zulkifal12 <mzulkifaluetp@gmail.com>
2022-11-02 21:17:47 +0100

    Product Updated

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -10,6 +10,7 @@ import { ProductService } from '../services/product.service';
 })
 export class SellerUpdateProductComponent implements OnInit {
   productdata: undefined | product;
+  productMessage:undefined |string;
   constructor(private route: ActivatedRoute, private product: ProductService) {}
 
   ngOnInit(): void {
@@ -21,5 +22,20 @@ export class SellerUpdateProductComponent implements OnInit {
     
   }
 
-  submit(data: any) {}
+  submit(data:product) {
+    if(this.productdata){
+      data.id = this.productdata.id;
+      this.product.updateProduct(data).subscribe((result)=>
+      {
+        if(result){
+        this.productMessage = "Product Updated Successfully."
+        }
+      });
+      setTimeout(()=>
+      {
+        this.productMessage = undefined;
+      },3000);
+    }
+
+  }
 }
